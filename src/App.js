31f68d5a2d4adf6ca8bf5238fd6336593e0d2482
Maggy Prajapati <mupraj10@gmail.com>
2017-12-23 03:19:26 -0500

    starting to change all to rebass components

--- src/App.js
+++ src/App.js
@@ -1,47 +1,60 @@
-import React, { Component } from 'react';
-import { HashRouter as Router, Route, Switch } from 'react-router-dom';
+import React, { Component } from "react";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 
-import Sidebar from './Sidebar'
+import { Provider, Divider, Flex, Box, Container } from "rebass";
 
-import Home from './Home';
-import AboutMe from './AboutMe';
-import Life from './Life'
-import Projects from './Projects';
-import ProjectCard from './ProjectCard';
-import Fun from './Fun';
-import Writing from './Writing';
+import Sidebar from "./Sidebar";
+
+import Home from "./Home";
+import AboutMe from "./AboutMe";
+import Life from "./Life";
+import Projects from "./Projects";
+import ProjectCard from "./ProjectCard";
+import Fun from "./Fun";
+import Writing from "./Writing";
 
 class App extends Component {
   render() {
-
-
     return (
+      <Provider
+        theme={{
+          font: '"Avenir Next", Helvetica, sans-serif',
+          fontSizes: [12, 16, 24, 36, 48, 72], 
+          colors:{
+            blue:'#3b5777',
+            yellow:'#c3a481'
+          }
+        }}
+      >
 
-      <article className="system serif">
-      <Router>
-        <div className="vh-100 dt w-100 tc bg-washed-green cover">
 
-          <Sidebar />
+      <Container  bg='blue' p={[2,3,5]} > 
+          <Router>
 
+            <Flex wrap>
+              <Sidebar />
 
-          <div className="fl w-100 w-70-m w-70-l">
-
+              <Box bg='yellow' w={[ 1, null, 3/4 ]}>
+             
                 <Switch>
-                <Route exact path="/" component={Home} />
-                <Route exact path="/me" component={AboutMe} />
-                <Route exact path='/life' component={Life} />
-                <Route exact path="/projects" component={Projects}  />
-                <Route exact path='/projects/:name' component={ProjectCard} />
-                <Route exact path="/writing" component={Writing} />
-                <Route exact path="/fun" component={Fun}  />
-              </Switch>
+                  <Route exact path="/" component={Home} />
+                  <Route exact path="/me" component={AboutMe} />
+                  <Route exact path="/life" component={Life} />
+                  <Route exact path="/projects" component={Projects} />
+                  <Route exact path="/projects/:name" component={ProjectCard} />
+                  <Route exact path="/writing" component={Writing} />
+                  <Route exact path="/fun" component={Fun} />
+                </Switch>
+              </Box>
+            </Flex>
 
-          </div>
-       </div>
-        </Router>
-      </article>
+          </Router>
+          </Container>
+      </Provider>
     );
   }
 }
 
 export default App;
+
+// #a7ba7b : #54437d  #4b4b4b : #c28988  #c3a481 : #3b5777
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import {  Link } from "react-router-dom";
 
+import {Box, BlockLink, Heading} from 'rebass';
+
 const titles = ['me','life', 'projects', 'fun', 'writing', 'contact'];
 const imgUrl = require('./images/me.jpg')
 
@@ -9,20 +11,21 @@ export default class Navbar extends Component {
     const btstyle =
     "f4 fw6 db pa2 no-underline tracked black";
     return (
-      <div className="fl w-100 w-30-m w-30-l vh-100-ns vh-50 shadow-2 ">
+      
+      <Box bg='yellow' w={[ 1, null, 1/4 ]}>
       <div className="measure tc pv4 ph2 ph3-s">
       <img src={imgUrl} className="br-100 h4 w4 dib ba b--black-05 pa2" title="Photo of a kitty staring at you" />
-        <div className="f2 f3-m ttu tracked fs-normal">Maggy Prajapati</div>
+        <Heading f={[2]} >MAGGY PRAJAPATI </Heading>
         <div className="pa2 lh-copy db-ns flex justify-center">
           {titles.map(title => {
             return(
-              <Link key={title} className={btstyle} to={`/${title}`}> <span className='pa2 br3 bg-animate hover-bg-pink'> {title} </span> </Link>
+               <BlockLink to={`/${title}`} is={Link} p={2}> {title} </BlockLink> 
             )
           })}
         
         </div>
       </div>
-      </div>
+      </Box>
     );
   }
 }
