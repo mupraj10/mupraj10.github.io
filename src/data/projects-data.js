3446b944d506ff22079d4a832a1f176ce942b613
Maggy Prajapati <mupraj10@gmail.com>
2018-04-14 18:34:59 -0400

    updated resume and typo

--- src/data/projects-data.js
+++ src/data/projects-data.js
@@ -1,5 +1,51 @@
 const projects = [
   {
+    id: 3,
+    name: 'Leslie Knope Quotes API',
+    link: 'leslie-knope',
+    completed: true,
+    picture: '/',
+    blurb:
+      'A simple API serving up Leslie Knope quotes from Parks and Recreation.',
+    info:
+      'A simple API serving up Leslie Knope quotes from Parks and Recreation.  It allows anyone to get a random quote or multiple quotes as a JSON Object.',
+      features: ['Users can make GET requests to recieve a random quote or a group of random quotes '],
+    github: 'https://github.com/mupraj10/leslie-knope-quotes',
+    demo: 'https://leslie-knope-quotes.herokuapp.com/',
+    youtube: false,
+    frameworks: [{ name: 'ExpressJS' }, { name: 'NodeJS' }]
+  },
+
+  {
+    id: 2,
+    name: 'Moodifs',
+    link: 'moodifs',
+    completed: false,
+    picture: '/',
+    features: ['Users can type in a mood and get a gif related to their feeling back', 'Users can see a gallery of all their previous moods and associated gifs'],
+    blurb:
+      'A web application that uses sentiment analysis to provide you with a daily gif based on your mood of the day.',
+    info:
+      'A progressive web application that uses sentiment analysis to provide you witl a daily gif based on your mood of the day. It also provides data visualization of your monthly, weekly moods to get a sense of how you are feeling!',
+    github: 'https://github.com/mupraj10/mooddif',
+    demo: false,
+    youtube: false,
+    frameworks: [
+      {
+        name: 'react',
+        link: ''
+      },
+      {
+        name: 'sentimentJS',
+        link: ''
+      },
+      {
+        name: 'giphyAPI',
+        link: ''
+      }
+    ]
+  },
+  {
     id: 0,
     name: 'BuJo',
     link: 'bujo',
@@ -72,72 +118,29 @@ const projects = [
       }
     ]
   },
-  {
-    id: 2,
-    name: 'Moodifs',
-    link: 'moodifs',
-    completed: false,
-    picture: '/',
-    features: ['Users can type in a mood and get a gif related to their feeling back', 'Users can see a gallery of all their previous moods and associated gifs'],
-    blurb:
-      'A progressive web application that uses sentiment analysis to provide you will a daily gif based on your mood of the day.',
-    info:
-      'A progressive web application that uses sentiment analysis to provide you will a daily gif based on your mood of the day. It also provides data visualization of your monthly, weekly moods to get a sense of how you are feeling!',
-    github: 'https://github.com/mupraj10/mooddif',
-    demo: false,
-    youtube: false,
-    frameworks: [
-      {
-        name: 'react',
-        link: ''
-      },
-      {
-        name: 'sentimentJS',
-        link: ''
-      },
-      {
-        name: 'giphyAPI',
-        link: ''
-      }
-    ]
-  },
-  {
-    id: 3,
-    name: 'Delayed',
-    link: 'delayed',
-    completed: false,
-    picture: '/',
-    blurb:
-      'A progressive web application uses MTA and user feedback to show if a train is delayed in real-time',
-    info:
-      'An application built to see if a train is delayed before the MTA annouces the delay via user communication',
-    github: 'https://github.com/mupraj10/mta-info',
-    features: ['currently still working on getting a frontend for this project'],
-    demo: false,
-    youtube: false,
-    frameworks: [
-      {
-        name: 'react',
-        link: ''
-      }
-    ]
-  },
-  {
-    id: 4,
-    name: 'Leslie Knope Quotes API',
-    link: 'leslie-knope',
-    completed: true,
-    picture: '/',
-    blurb:
-      'A simple API serving up Leslie Knope quotes from Parks and Recreation.',
-    info:
-      'A simple API serving up Leslie Knope quotes from Parks and Recreation.  It allows anyone to get a random quote or multiple quotes as a JSON Object.',
-      features: ['Users can make GET requests to recieve a random quote or a group of random quotes '],
-    github: 'https://github.com/mupraj10/leslie-knope-quotes',
-    demo: 'https://leslie-knope-quotes.herokuapp.com/',
-    youtube: false,
-    frameworks: [{ name: 'ExpressJS' }, { name: 'NodeJS' }]
-  }
+  
 ];
 
 export default projects;
+
+// {
+//   id: 3,
+//   name: 'Delayed',
+//   link: 'delayed',
+//   completed: false,
+//   picture: '/',
+//   blurb:
+//     'A progressive web application uses MTA and user feedback to show if a train is delayed in real-time',
+//   info:
+//     'An application built to see if a train is delayed before the MTA annouces the delay via user communication',
+//   github: 'https://github.com/mupraj10/mta-info',
+//   features: ['currently still working on getting a frontend for this project'],
+//   demo: false,
+//   youtube: false,
+//   frameworks: [
+//     {
+//       name: 'react',
+//       link: ''
+//     }
+//   ]
+// }
